ab9796409ac418b02870d1bb837c11e4d6660c37
khasanjon <khasanjon.dev@gmail.com>
2023-11-16 12:00:15 +0500

    update some files

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import User, AnonymousUser
+from django.db.models import Model, ForeignKey, SET, TextField, DateTimeField
 
-# Create your models here.
+
+class Message(Model):
+    sender_user = ForeignKey(User, SET(AnonymousUser.id), 'sender')
+    receiver_user = ForeignKey(User, SET(AnonymousUser.id), 'receiver')
+    message = TextField()
+    timestamp = DateTimeField(auto_now_add=True)
