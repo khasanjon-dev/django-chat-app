ab930272d4d5ec46ea21b537b16d2d618cc6d179
khasanjon <khasanjon.dev@gmail.com>
2023-11-16 11:46:54 +0500

    update some files

--- root/asgi.py
+++ root/asgi.py
@@ -1,7 +1,12 @@
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
-
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_app,
+    }
+)
