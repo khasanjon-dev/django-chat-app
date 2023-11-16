443a3f273ee00a0fe1517a0285d62139e6392041
khasanjon <khasanjon.dev@gmail.com>
2023-11-16 12:02:05 +0500

    update some files

--- chats/models.py
+++ chats/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, AnonymousUser
-from django.db.models import Model, ForeignKey, SET, TextField, DateTimeField
+from django.db.models import Model, ForeignKey, SET, TextField, DateTimeField, CharField
 
 
 class Message(Model):
@@ -7,3 +7,12 @@ class Message(Model):
     receiver_user = ForeignKey(User, SET(AnonymousUser.id), 'receiver')
     message = TextField()
     timestamp = DateTimeField(auto_now_add=True)
+
+
+class Room(Model):
+    sender_user = ForeignKey(User, SET(AnonymousUser.id), 'room_sender')
+    receiver_user = ForeignKey(User, SET(AnonymousUser.id), 'room_receiver')
+    room_name = CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.room_name
