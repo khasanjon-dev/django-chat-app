b23f68b6cc153c1c2bb63b57a08024bd732a13d0
khasanjon <khasanjon.dev@gmail.com>
2023-11-16 13:29:56 +0500

    add chatroom view

---
+++ chats/consumers.py
@@ -0,0 +1,43 @@
+import json
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
+
+from chats.models import Message
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def save_message(self, message, sender, receiver):
+        sender_user = User.objects.get(id=sender)
+        receiver_user = User.objects.get(id=receiver)
+        data = {
+            'message': message,
+            'sender_user': sender_user,
+            'receiver_user': receiver_user
+        }
+        new_message = Message.objects.create(**data)
+        new_message.save()
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        sender = text_data_json['sender']
+        receiver = text_data_json['receiver']
+        sender_name = text_data_json['sender_name']
+        self.save_message(mes)
--- chats/urls.py
+++ chats/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from chats.views import GetAllUsers, ChatRoom
+
 urlpatterns = [
-    # path('', chat_views)
+    path('rooms/', GetAllUsers.as_view(), name='rooms'),
+    path('rooms/<str:room_name>', ChatRoom.as_view(), name='room'),
 ]
+
