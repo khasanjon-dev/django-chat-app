8d856b6f0a153806bd78c6e0c114b87868ab75a7
khasanjon <khasanjon.dev@gmail.com>
2023-11-16 13:37:22 +0500

    add chatroom view

--- root/asgi.py
+++ root/asgi.py
@@ -1,12 +1,23 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from chats import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         'http': django_asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    routing.websocket_urlpatterns
+                )
+            )
+        ),
     }
 )
