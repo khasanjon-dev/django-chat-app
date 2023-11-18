8c3375fa445aeda1117de66e84202c735638addc
khasanjon <khasanjon.dev@gmail.com>
2023-11-18 12:51:23 +0500

    udpate some files

--- root/asgi.py
+++ root/asgi.py
@@ -2,13 +2,22 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from chats import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application(),
+        'http': django_asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    routing.websocket_urlpatterns
+                )
+            )
+        )
     }
 )
