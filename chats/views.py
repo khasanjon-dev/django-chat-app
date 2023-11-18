2603fcc177c6509125b18f7326025cccd9506312
khasanjon <khasanjon.dev@gmail.com>
2023-11-18 09:38:13 +0500

    update some action

--- chats/views.py
+++ chats/views.py
@@ -1,1 +1,13 @@
+from django.shortcuts import render
+
+from chats.models import Room
+
+
+def index_view(request):
+    detail = {
+        'rooms': Room.objects.all()
+    }
+    return render(request, 'index.html', detail)
+
+
 
