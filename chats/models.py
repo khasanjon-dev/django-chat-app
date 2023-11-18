d4489bb6187be4eb316d069be93f085c859c8c04
khasanjon <khasanjon.dev@gmail.com>
2023-11-18 09:24:57 +0500

    add Room model

--- chats/models.py
+++ chats/models.py
@@ -1,2 +1,23 @@
+from django.contrib.auth.models import User
+from django.db.models import Model, CharField, ManyToManyField
+
+
+class Room(Model):
+    name = CharField(max_length=250)
+    online = ManyToManyField(User, blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
 
 
