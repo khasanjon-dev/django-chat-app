e0c67995987d4818408758e3386de030ca387653
khasanjon <khasanjon.dev@gmail.com>
2023-11-19 15:59:53 +0500

    update some files

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,5 +1,8 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
+
+from chats.models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -15,4 +18,23 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
+    def disconnect(self, close_code):
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
+    # save message in the database
+    def save_message(self, message, sender, receiver):
+        sender = User.objects.get(id=sender)
+        receiver = User.objects.get(id=receiver)
+        data = {
+            'message': message,
+            'sender': sender,
+            'receiver': receiver
+        }
+        new_message = Message.objects.create(**data)
+        new_message.save()
+
+    # receive message frome WebSocket
