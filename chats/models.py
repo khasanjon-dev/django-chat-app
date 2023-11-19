85d9c5663a0fac3022a804c3042d9b0607398514
khasanjon <khasanjon.dev@gmail.com>
2023-11-19 16:34:58 +0500

    update some files

--- chats/models.py
+++ chats/models.py
@@ -6,15 +6,15 @@ class Message(Model):
     message = TextField()
     timestamp = DateTimeField(auto_now_add=True)
     # relationships
-    sender = ForeignKey(User, SET(AnonymousUser.id), 'sender')
-    receiver = ForeignKey(User, SET(AnonymousUser.id), 'receiver')
+    sender = ForeignKey(User, SET(AnonymousUser.id), 'senders')
+    receiver = ForeignKey(User, SET(AnonymousUser.id), 'receivers')
 
 
 class Room(Model):
     name = CharField(max_length=250, unique=True)
     # relationships
-    sender = ForeignKey(User, SET(AnonymousUser.id), 'sender')
-    receiver = ForeignKey(User, SET(AnonymousUser.id), 'receiver')
+    sender = ForeignKey(User, SET(AnonymousUser.id), 'room_senders')
+    receiver = ForeignKey(User, SET(AnonymousUser.id), 'room_receivers')
 
     def __str__(self):
         return self.name
