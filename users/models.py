5a87bbd42025ba5bf5cf2ecb098409c8067b83e0
khasanjon <khasanjon.dev@gmail.com>
2023-11-22 11:49:36 +0500

    update some files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.db.models import (CharField, DateTimeField, EmailField)
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = CharField(max_length=150, null=True, blank=True)
+    email = EmailField(unique=True, blank=True)
+
+    updated_at = DateTimeField(auto_now=True, null=True)
+    USERNAME_FIELD = 'email'
