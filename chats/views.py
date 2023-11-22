c4c6e94b24555500acb632c49514bdaa50a8ffbd
khasanjon <khasanjon.dev@gmail.com>
2023-11-22 14:35:37 +0500

    update some actions

--- chats/views.py
+++ chats/views.py
@@ -1,16 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from users.models import User
 
 
 def index(request):
-    users = User.objects.all()
+    users = User.objects.order_by('-date_joined')
     context = {
         'users': users
     }
     return render(request, 'index.html', context)
 
 
+@login_required
 def chat(request):
     users = User.objects.all()
     context = {
