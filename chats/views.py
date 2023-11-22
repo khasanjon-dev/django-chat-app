3eb6f2c88ecdba9c7e07356649c8ee2e6e5e1ae3
khasanjon <khasanjon.dev@gmail.com>
2023-11-22 14:30:57 +0500

    update some actions

--- chats/views.py
+++ chats/views.py
@@ -5,11 +5,15 @@ from users.models import User
 
 def index(request):
     users = User.objects.all()
-    detail = {
+    context = {
         'users': users
     }
-    return render(request, 'index.html', detail)
+    return render(request, 'index.html', context)
 
 
 def chat(request):
-    return render(request, 'chats/chat.html')
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'chats/chat.html', context)
