6ac0c5ac377a50b2f6a793b7a4dfcf96ed5058b6
khasanjon <khasanjon.dev@gmail.com>
2023-11-22 11:42:35 +0500

    update some files

--- root/urls.py
+++ root/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chats.urls'))
+    path('', include('chats.urls')),
+    path('user/', include('users.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
